36ec953a0efb682ad90cf71d7e1bf449fd4745d0
Chandu J S <app.coder@outlook.in>
2019-09-25 20:14:07 +0400

    sentry dsn added

--- lib/src/constants/keys.dart
+++ lib/src/constants/keys.dart
@@ -1,6 +1,6 @@
-/// All types of API keys and stuffs belong here.
+/// All types of API keys belong here.
 mixin KeysConstant {
   static const String spotify = '';
   static const String itunes = '';
-  static const String sentryDsn = '';
+  static const String sentryDsn = 'https://e4231d07b2d64dc5a8f58d4529f758c8@sentry.io/1760652';
 }
