ba12bfa8bb50cc4ac2d7c1b82a88821d495f532e
Chandu J S <app.coder@outlook.in>
2019-10-03 21:51:11 +0400

    WIP: recent thumb section

---
+++ lib/src/models/recent_thumb.dart
@@ -0,0 +1,11 @@
+class RecentThumb {
+  String trackName;
+  String artistName;
+  String albumArt;
+
+  RecentThumb({
+    this.albumArt,
+    this.artistName,
+    this.trackName,
+  });
+}
--- lib/src/views/pages/root.dart
+++ lib/src/views/pages/root.dart
@@ -1,8 +1,46 @@
 import 'package:flutter/material.dart';
 
+import 'package:music_stories/src/models/recent_thumb.dart';
+
 class RootPage extends StatelessWidget {
+  final List<RecentThumb> recentThumbs = [
+    RecentThumb(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 500,
+              child: ListView.builder(
+                controller: PageController(
+                  initialPage: 0,
+                  viewportFraction: 0.7,
+                ),
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      left: 8,
+                      top: 8,
+                      bottom: 8,
+                      right: (index == (10 - 1)) ? 8 : 0
+                    ),
+                    child: Container(
+                      color: Colors.black,
+                      width: MediaQuery.of(context).size.width / 1.3,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
